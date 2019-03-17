b967c9620bc29d4ab44e2f7f11199e34e830694e
Jesse V <jesvanv@gmail.com>
2019-03-17 18:18:49 -0400

    Add customer and ws inquiry tabs

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,42 @@
 var bottomNav = document.getElementsByClassName('bottom-nav')[0];
+var widerArea = document.getElementById('bottom-wide-area')
 var popupOverlay = document.getElementById('popup-overlay');
 var encoded = "fDfFx@FfSfjSZ61d7jp.76l"
 var key = "156vFDJLPVgwrjRyu89fXmS2Na4HW3qcQkOZAGsdboMnl0EBxUTYtz7hCIeiKp"
 var shift = encoded.length
 var link = ""
-for (i=0; i<encoded.length; i++) {
-  if (key.indexOf(encoded.charAt(i)) == -1) {
-    ltr = encoded.charAt(i)
-    link += (ltr)
+
+  for (i=0; i<encoded.length; i++) {
+    if (key.indexOf(encoded.charAt(i)) == -1) {
+      ltr = encoded.charAt(i)
+      link += (ltr)
+    }
+    else {
+      ltr = (key.indexOf(encoded.charAt(i)) - shift + key.length) % key.length
+      link += (key.charAt(ltr))
+    }
   }
-  else {
-    ltr = (key.indexOf(encoded.charAt(i)) - shift + key.length) % key.length
-    link += (key.charAt(ltr))
-  }
-}
+
+var widerAreaContent = "<a href='mailto:" + link + "?subject=Wholesale Inquiry'>" +
+      "<div class='contact wholesale'>" +
+        "<h1>Wholesale inquiries?<h1>" +
+        "<p>Let us know!</p>" +
+      "</div>" +
+    "</a>" +
+
+      "<a href='mailto:" + link + "?subject=Customer Inquiry'>" +
+      "<div class='contact consumer'>" +
+        "<h1>Consumer inquiries?</h1>" +
+        "<p>Happy to help!</p>" +
+      "</div>" +
+      "<h2 class='bottom-head'>Chasing Our Tails is committed to giving pets everything they need and crave as a healthy addition to their daily diet!</h2>"
+
+widerArea.innerHTML = widerAreaContent;
+
 var emailLink = document.createElement('a');
 emailLink.innerHTML = "<a href='mailto:" + link + "' id='email' class='email'>Email Us Today!</a>";
 bottomNav.append(emailLink)
+
 
 var showPopup = function() {
   var popupOverlayClasses = popupOverlay.classList;
