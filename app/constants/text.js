e896f1c6e9d50f8425784e5d5ea378fdb12ba469
Jesse V <jesvanv@gmail.com>
2020-03-01 18:48:30 -0500

    Address update.

--- app/constants/text.js
+++ app/constants/text.js
@@ -1,6 +1,6 @@
 export const COMPANY_NAME ='Chasing Our Tails';
-export const MISSION_DESCRIPTION = 'Our line is single ingredient and preservative-free; your pets deserve the best. We NEVER use fillers, meat meals, animal by-products, or anything artificial in our treats. We manufacture all of our items in our own facility and then package them on-site to ensure peak freshness.  All of our treats are single ingredient US sourced and US made products. Gluten and grain free, our treats and chews are great for pets with allergies.';
+export const MISSION_DESCRIPTION = 'Our line is single ingredient and preservative-free; your pets deserve the best. We NEVER use fillers, meat meals, animal by-products, or anything artificial in our treats. We manufacture all of our items in our own facility and then package them on-site to ensure peak freshness. All of our treats are single ingredient US sourced and US made products. Gluten and grain free, our treats and chews are great for pets with allergies.';
 export const INTRO_TEXT = 'Our line has a variety of treats and chews to engage, occupy, and satisfy pets of all sizes. These are just a few of the products we carry to support your pet\'s healthy lifestyle.';
 export const PROMO_TEXT = 'Knee caps, lamb trachea, cow ears, chicken feet, and piggy sticks, oh my! (And many more!) We have what you need to satisfy pups of all sizes!';
 export const BUTTON_TEXT = 'Email Elena today for more details';
-export const ADDRESS = '28 Hampshire Drive • Hudson, NH • 03051';
+export const ADDRESS = '218 N Jefferson St • Minneota, MN • 56264';
